a897839283228dac4a2003a295f049e20fc8a7b0
Ralo <gpfernandezflorio@gmail.com>
2025-03-03 20:39:29 -0300

    Ajustes: defino una función constructora

--- src/ajustes.js
+++ src/ajustes.js
@@ -3,27 +3,31 @@ Mila.Modulo({
 });
 
 Mila.Ajustes.nuevo = function(ajustes) {
-  const resultado = {
-    _claves: Object.keys(ajustes)
-  };
-  for (let clave of resultado._claves) {
-    resultado[`_${clave}`] = Object.assign({valorActual: ajustes[clave].inicial}, ajustes[clave]);
-    resultado[clave] = function() {
-      return resultado[`_${clave}`].valorActual;
+  return new Mila.Ajustes._Ajustes(ajustes);
+};
+
+Mila.Ajustes._Ajustes = function Ajustes(ajustes) {
+  this._claves = Object.keys(ajustes);
+  for (let clave of this._claves) {
+    this[`_${clave}`] = Object.assign({valorActual: ajustes[clave].inicial}, ajustes[clave]);
+    this[clave] = function() {
+      return this[`_${clave}`].valorActual;
     }
   }
-  resultado.Ajustar_A_ = function(clave, valor) {
-    resultado[`_${clave}`].valorActual = valor;
-  };
-  resultado.todos = function() {
-    const todosLosAjustes = {};
-    for (let clave of this._claves) {
-      todosLosAjustes[clave] = this[clave]();
-    }
-    return todosLosAjustes;
-  };
-  resultado.existe_ = function(clave) {
-    return this._claves.includes(clave);
-  };
-  return resultado;
+};
+
+Mila.Ajustes._Ajustes.prototype.Ajustar_A_ = function(clave, valor) {
+  this[`_${clave}`].valorActual = valor;
+};
+
+Mila.Ajustes._Ajustes.prototype.todos = function() {
+  const todosLosAjustes = {};
+  for (let clave of this._claves) {
+    todosLosAjustes[clave] = this[clave]();
+  }
+  return todosLosAjustes;
+};
+
+Mila.Ajustes._Ajustes.prototype.existe_ = function(clave) {
+  return this._claves.includes(clave);
 };
