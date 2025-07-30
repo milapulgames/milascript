3be57a6197dbd4b419271671a62fdfdbe9791cb8
Olimar <gpfernandezflorio@gmail.com>
2025-07-30 11:20:06 -0300

    Profundizo la definición del módulo AST separando entre nodos gramaticales (tokens y expresiones regulares) y nodos de un AST particular

--- src/ast.js
+++ src/ast.js
@@ -4,15 +4,6 @@ Mila.Modulo({
   necesita:["base","tipo","lista","geometria"]
 });
 
-Mila.AST.TipoNodoAST = Mila.Tipo.Variante("TipoNodoAST",
-  ["Nodo","Token","Identificador",
-    "Comando","Expresion",
-    "DefinicionProcedimiento","DefinicionFuncion",
-    "BloqueComando","Operador",
-    "Asignacion"
-  ]
-);
-
 Mila.Tipo.Registrar({
   nombre:'Cursor',
   es: {
@@ -26,7 +17,53 @@ Mila.AST.UbicacionNodo = Mila.Tipo.Alias("UbicacionNodo",
   Mila.Tipo.O([Mila.Tipo.Nada, Mila.Tipo.Punto, Mila.Tipo.Cursor])
 );
 
-Mila.AST._NodoAST = function NodoAST(id, tipoNodo) {
+/*
+  Nodo
+  - NodoGramatical
+    - Token (identificadores, números, otros símbolos)
+    - RegEx (símbolos no terminales y expresiones regulares como pipe, kleene, etc.)
+  - NodoAST
+*/
+
+Mila.AST._Nodo = function Nodo() {}; // Privado, no lo registro como tipo de Mila
+
+Mila.AST._NodoGramatical = function NodoGramatical() {};
+Object.setPrototypeOf(Mila.AST._NodoGramatical.prototype, Mila.AST._Nodo.prototype);
+
+Mila.Tipo.Registrar({
+  nombre: "NodoGramatical",
+  prototipo: Mila.AST._NodoGramatical
+});
+
+Mila.AST._Token = function Token(clase, contenido="") {
+  this.clase = clase;
+  this.contenido = contenido;
+};
+Object.setPrototypeOf(Mila.AST._Token.prototype, Mila.AST._NodoGramatical.prototype);
+
+Mila.Tipo.Registrar({
+  nombre: "Token",
+  es: 'esTokenAtómico',
+  prototipo: Mila.AST._Token,
+  subtipoDe: Mila.Tipo.NodoGramatical,
+  strInstancia: x => `T( ${x.contenido.length > 0 ? x.contenido : x.clase} )`
+});
+
+Mila.AST._RegEx = function RegEx(clase) {
+  this.clase = clase;
+  this.hijos = {};
+  this.campos = {};
+};
+Object.setPrototypeOf(Mila.AST._RegEx.prototype, Mila.AST._NodoGramatical.prototype);
+
+Mila.Tipo.Registrar({
+  nombre: "RegEx",
+  es: 'esTokenRegEx',
+  prototipo: Mila.AST._RegEx,
+  subtipoDe: Mila.Tipo.NodoGramatical
+});
+
+Mila.AST._NodoAST = function NodoAST(id, tipoNodo, textoOriginal) {
   this.id = id;
   this.tipoNodo = tipoNodo;
   this.nivel = 0;
@@ -34,12 +71,34 @@ Mila.AST._NodoAST = function NodoAST(id, tipoNodo) {
   this.clausura = [];
   this.hijos = {};
   this.campos = {};
+  this.textoOriginal = textoOriginal;
 };
+Object.setPrototypeOf(Mila.AST._NodoAST.prototype, Mila.AST._Nodo.prototype);
 
 Mila.Tipo.Registrar({
   nombre: "NodoAST",
   prototipo: Mila.AST._NodoAST,
   strInstancia: (x) => x.id
+});
+
+Mila.Tipo.Registrar({
+  nombre:'MapaNodosGramaticales',
+  es: function(elemento) {
+    return elemento.clavesDefinidas().todosCumplen_(x =>
+      elemento[x].esDeTipo_(Mila.Tipo.NodoGramatical) ||
+      elemento[x].esDeTipo_(Mila.Tipo.ListaDe_(Mila.Tipo.NodoGramatical))
+    );
+  },
+  inferible: false
+});
+
+Mila.Tipo.Registrar({
+  nombre:'AtributosNodoGramatical',
+  es: {
+    "?hijos":Mila.Tipo.MapaNodosGramaticales,
+    "?campos":Mila.Tipo.Registro
+  },
+  inferible: false
 });
 
 Mila.Tipo.Registrar({
@@ -60,15 +119,28 @@ Mila.Tipo.Registrar({
     "?tipoNodo":Mila.Tipo.Texto,
     "?hijos":Mila.Tipo.MapaNodosAST,
     "?ubicacion":Mila.Tipo.UbicacionNodo,
-    "?campos":Mila.Tipo.Registro
+    "?campos":Mila.Tipo.Registro,
+    "?textoOriginal":Mila.Tipo.Texto
   },
   inferible: false
 });
 
+Mila.AST.nuevoToken = function(clase, contenido="") {
+  return new Mila.AST._Token(clase, contenido);
+};
+
+Mila.AST.nuevaRegEx = function(clase, atributos={}) {
+  let nuevaRegEx = new Mila.AST._RegEx(clase);
+  nuevaRegEx.CambiarHijosA_('hijos' in atributos ? atributos.hijos : {});
+  nuevaRegEx.CambiarCamposA_('campos' in atributos ? atributos.campos : {});
+  return nuevaRegEx;
+};
+
 Mila.AST.nuevoNodo = function(atributos={}) {
   let nuevoNodo = new Mila.AST._NodoAST(
     'id' in atributos ? atributos.id : Mila.AST.nuevoIdPara_("Nodo"),
-    'tipoNodo' in atributos ? atributos.tipoNodo : Mila.AST.TipoNodoAST.Nodo
+    'tipoNodo' in atributos ? atributos.tipoNodo : "Nodo",
+    'textoOriginal' in atributos ? atributos.textoOriginal : Mila.Nada
   );
   nuevoNodo.CambiarHijosA_('hijos' in atributos ? atributos.hijos : {});
   nuevoNodo.CambiarUbicacionA_('ubicacion' in atributos ? atributos.ubicacion : Mila.Nada);
@@ -80,7 +152,7 @@ Mila.AST.nuevoNodoDeTipo_ = function(tipoNodo) {
   return Mila.AST.nuevoNodo({tipoNodo});
 };
 
-Mila.AST._NodoAST.prototype.CambiarHijosA_ = function(nuevosHijos) {
+Mila.AST._Nodo.prototype.CambiarHijosA_ = function(nuevosHijos) {
   for (let hijoViejo in this.hijos) {
     if (
       !nuevosHijos.defineLaClave_(hijoViejo) ||
@@ -107,10 +179,15 @@ Mila.AST._NodoAST.prototype.CambiarHijosA_ = function(nuevosHijos) {
     }
   }
   this.hijos = nuevosHijos;
+};
+
+Mila.AST._NodoAST.prototype.CambiarHijosA_ = function(nuevosHijos) {
+  // super.CambiarHijosA_(nuevosHijos);
+  Mila.AST._Nodo.prototype.CambiarHijosA_.call(this, nuevosHijos);
   this.NivelarHijos();
 };
 
-Mila.AST._NodoAST.prototype.CambiarHijo_A_ = function(clave, nuevoHijo) {
+Mila.AST._Nodo.prototype.CambiarHijo_A_ = function(clave, nuevoHijo) {
   if (nuevoHijo.esUnaLista() && this[clave].length == 0) {
     this[clave] = function(i) {
       if (Mila.Tipo.esNada(i)) {
@@ -125,6 +202,11 @@ Mila.AST._NodoAST.prototype.CambiarHijo_A_ = function(clave, nuevoHijo) {
     }
   }
   this.hijos[clave] = nuevoHijo;
+};
+
+Mila.AST._NodoAST.prototype.CambiarHijo_A_ = function(clave, nuevoHijo) {
+  // super.CambiarHijo_A_(clave, nuevoHijo);
+  Mila.AST._Nodo.prototype.CambiarHijo_A_.call(this, clave, nuevoHijo);
   this.NivelarHijo_(clave);
 };
 
@@ -136,11 +218,11 @@ Mila.AST._NodoAST.prototype.CambiarNivelA_ = function(nuevoNivel) {
   this.nivel = nuevoNivel;
 };
 
-Mila.AST._NodoAST.prototype.CambiarCampo_A_ = function(clave, nuevoValor) {
+Mila.AST._Nodo.prototype.CambiarCampo_A_ = function(clave, nuevoValor) {
   this.campos[clave] = nuevoValor;
 };
 
-Mila.AST._NodoAST.prototype.CambiarCamposA_ = function(nuevosCampos) {
+Mila.AST._Nodo.prototype.CambiarCamposA_ = function(nuevosCampos) {
   for (let campoViejo in this.campos) {
     if (!nuevosCampos.defineLaClave_(campoViejo)) {
       delete this[campoViejo];
@@ -175,7 +257,7 @@ Mila.AST._NodoAST.prototype.NivelarHijo_ = function(clave) {
   });
 };
 
-Mila.AST._NodoAST.prototype.fold = function(mapaDeFunciones) {
+Mila.AST._Nodo.prototype.fold = function(mapaDeFunciones) {
   let clave = this.tipoNodo.aTexto();
   if (!mapaDeFunciones.defineLaClave_(clave)) {
     clave = "Nodo";
