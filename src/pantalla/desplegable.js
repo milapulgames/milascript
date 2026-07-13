35b9b15348a434a9edd191a646cf39fdb8a5a046
Olimar <gpfernandezflorio@gmail.com>
2026-07-13 17:35:13 -0300

    Elimino el panel desplegable y dejo únicamente el constructor genérico de elementos desplegables

--- src/pantalla/desplegable.js
+++ src/pantalla/desplegable.js
@@ -3,6 +3,23 @@ Mila.Módulo({
 });
 
 Mila.Tipo.Registrar({
+  nombre:'AtributosDesplegable',
+  es: {
+    // El elmento visual a convertir en desplegable.
+    "elementoVisual":Mila.Tipo.ElementoVisual,
+    // ¿Inicia desplegado? Por defecto, no.
+    "?desplegado":Mila.Tipo.Booleano,
+    // Función a ejecutar la primera vez que se despliega.
+    "?PrimerDespliegue":Mila.Tipo.Funcion,
+    // Función a ejecutar cada vez que se despliega.
+    "?Desplegar":Mila.Tipo.Funcion,
+    // Función a ejecutar cada vez que se pliega.
+    "?Plegar":Mila.Tipo.Funcion
+  },
+  inferible: false
+});
+
+Mila.Tipo.Registrar({
   nombre:'Desplegable',
   es:{
     Desplegar:Mila.Tipo.Funcion,
@@ -11,38 +28,65 @@ Mila.Tipo.Registrar({
   }
 });
 
-Mila.Pantalla.desplegableAPartirDe_ = function(contenedor, funcionDeContenido) {
+Mila.Pantalla.desplegableAPartirDe_ = function(atributos) {
   Mila.Contrato({
     Proposito: [
-      "Describir al contenedor dado tras convertirlo en un desplegable con el contenido generado por la función dada",
+      "Describir al un nuevo desplegable a partir de los atributos dados.",
       Mila.Tipo.Desplegable
     ],
-    Precondiciones:[
-      "El primer parámetro es un contenedor (sabe responder al mensaje CambiarElementosA_)",
-      contenedor.sabeResponder_('CambiarElementosA_')
-    ],
     Parametros: [
-      [contenedor, Mila.Tipo.ElementoVisual],
-      [funcionDeContenido, Mila.Tipo.Funcion]
-      // [contenido, Mila.Tipo.O([
-      //   Mila.Tipo.ElementoVisual,
-      //   Mila.Tipo.ListaDe_(Mila.Tipo.ElementoVisual)
-      // ])]
+      [atributos, Mila.Tipo.AtributosDesplegable]
     ]
   });
-  contenedor._desplagado = false;
-  contenedor.Desplegar = function() {
-    if (this._desplagado) { return; }
-    contenedor.CambiarElementosA_(funcionDeContenido());
-    this._desplagado = true;
+  const elementoVisual = atributos.elementoVisual;
+  elementoVisual._desplegado = ('desplegado' in atributos) && atributos.desplegado;
+  let primerDespliegue = Mila.Nada;
+  if (elementoVisual._desplegado) {
+    if ('PrimerDespliegue' in atributos) {
+      atributos.PrimerDespliegue();
+    }
+  } else {
+    elementoVisual.Ocultar();
+    if ('PrimerDespliegue' in atributos) {
+      primerDespliegue = atributos.PrimerDespliegue;
+    }
+  }
+  const Deplegar = function() {
+    if (elementoVisual._desplegado) { return; }
+    elementoVisual.Mostrar();
+    elementoVisual._desplegado = true;
+    Mila.Pantalla._SolicitarRedimensión();
   };
-  contenedor.Plegar = function() {
-    if (!this._desplagado) { return; }
-    contenedor.CambiarElementosA_([]);
-    this._desplagado = false;
+  const Plegar = function() {
+    if (!elementoVisual._desplegado) { return; }
+    elementoVisual.Ocultar();
+    elementoVisual._desplegado = false;
+    Mila.Pantalla._SolicitarRedimensión();
   };
-  contenedor.desplegado = function() {
-    return this._desplagado;
+  elementoVisual.desplegado = function() {
+    return elementoVisual._desplegado;
   };
-  return contenedor;
+  elementoVisual.Desplegar = ('Desplegar' in atributos)
+    ? function() {
+      atributos.Desplegar();
+      Deplegar();
+    }
+    : Deplegar
+  ;
+  if (primerDespliegue.esAlgo()) {
+    const despliegueNormal = elementoVisual.Desplegar;
+    elementoVisual.Desplegar = function() {
+      primerDespliegue();
+      despliegueNormal();
+      elementoVisual.Desplegar = despliegueNormal;
+    };
+  }
+  elementoVisual.Plegar = ('Plegar' in atributos)
+    ? function() {
+      atributos.Plegar();
+      Plegar();
+    }
+    : Plegar
+  ;
+  return elementoVisual;
 };
