f3233920571ed00d5ea4d90a21a125204763faf8
Olimar <gpfernandezflorio@gmail.com>
2026-09-06 19:40:24 -0300

    Agrego la posibilidad de implementar un lienzo a partir de nodos Svg (en lugar de mediante un canvas) y uso este nuevo modo para el escenario

--- src/lienzo.js
+++ src/lienzo.js
@@ -4,6 +4,19 @@ Mila.Módulo({
   necesita:["tipo","pantalla"]
 });
 
+Mila.Lienzo.ModoHtmlLienzo = Mila.Tipo.Variante("ModoHtmlLienzo",
+  ["Canvas","Svg"]
+);
+
+Mila.Lienzo.ClaveModoHtmlLienzo = Mila.Tipo.Registrar({
+  nombre:'ClaveModoHtmlLienzo',
+  subtipoDe:Mila.Tipo.Texto,
+  es: function(elemento) {
+    return elemento in Mila.Lienzo.ModoHtmlLienzo;
+  },
+  inferible: false
+});
+
 Mila.Lienzo.nuevo = function(contentido=[]) {
   Mila.Contrato({
     Propósito: [
@@ -21,11 +34,24 @@ Mila.Lienzo._Lienzo = function Lienzo(contentido=[]) {
   this._contenido = contentido;
   this._dimensiones = Mila.Geometria.rectánguloCompleto();
   this._grosorBorde = 1;
+  this._modoHtml = Mila.Lienzo.ModoHtmlLienzo.Canvas;
+};
+
+Mila.Lienzo._Lienzo.prototype.CambiarModoHtmlA_ = function(modo) {
+  Mila.Contrato({
+    Propósito: "Cambiar el modo html de este lienzo por el dado.",
+    Parámetros: [
+      [modo, Mila.Tipo.O([Mila.Tipo.ModoHtmlLienzo, Mila.Tipo.ClaveModoHtmlLienzo])]
+    ]
+  });
+  this._modoHtml = modo.esDeTipo_(Mila.Tipo.ClaveModoHtmlLienzo)
+    ? Mila.Lienzo.ModoHtmlLienzo[modo] : modo
+  ;
 };
 
 Mila.Lienzo._Lienzo.prototype.PlasmarEnHtml = function(nodoMadre) {
   Mila.Contrato({
-    Propósito: "Plasmar este lienzo en el documento html como hijo del nodo dado",
+    Propósito: "Plasmar este lienzo en el documento html como hijo del nodo dado.",
     Precondiciones: [
       "Se está ejecutando en un navegador.", Mila.entorno().enNavegador(),
       "No se inicializó el nodo HTML de este lienzo antes.", !('_nodoHtml' in this)
@@ -34,12 +60,31 @@ Mila.Lienzo._Lienzo.prototype.PlasmarEnHtml = function(nodoMadre) {
       nodoMadre // Tipo nodo dom
     ]
   });
-  this._nodoHtml = document.createElement('canvas');
-  this._nodoHtml.style.margin = '0';
-  this._nodoHtml.style.padding = '0';
-  this._nodoHtml.style.position = 'absolute';
-  this._contexto = this._nodoHtml.getContext('2d');
-  nodoMadre.appendChild(this._nodoHtml);
+  if (this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Canvas)) {
+    this._nodoHtml = document.createElement('canvas');
+    this._nodoHtml.style.margin = '0';
+    this._nodoHtml.style.padding = '0';
+    this._contexto = this._nodoHtml.getContext('2d');
+    nodoMadre.appendChild(this._nodoHtml);
+  } else { // this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Svg)
+    this._nodoHtml = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+    this._nodoHtml.style.position = 'absolute';
+    nodoMadre.appendChild(this._nodoHtml);
+  }
+};
+
+Mila.Lienzo._Lienzo.prototype.QuitarDelHtml = function() {
+  Mila.Contrato({
+    Proposito: "Quitar este lienzo del documento html",
+    Precondiciones: [
+      "Se está ejecutando en el navegador",
+      Mila.entorno().enNavegador()
+    ]
+  });
+  if ('_nodoHtml' in this) {
+    this._nodoHtml.remove();
+    delete this._nodoHtml;
+  }
 };
 
 Mila.Lienzo._Lienzo.prototype.CambiarContenidoA_ = function(nuevoContenido) {
@@ -97,8 +142,15 @@ Mila.Lienzo._Lienzo.prototype.Redimensionar = function(rectángulo) {
   if ('_nodoHtml' in this) {
     this._nodoHtml.style.left = `${rectángulo.x}px`;
     this._nodoHtml.style.top = `${rectángulo.y}px`;
-    this._nodoHtml.width = rectángulo.ancho-2*this._grosorBorde;
-    this._nodoHtml.height = rectángulo.alto-2*this._grosorBorde;
+    const ancho = rectángulo.ancho-2*this._grosorBorde;
+    const alto = rectángulo.alto-2*this._grosorBorde;
+    if (this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Canvas)) {
+      this._nodoHtml.width = ancho;
+      this._nodoHtml.height = alto;
+    } else { // this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Svg)
+      this._nodoHtml.setAttribute("width",`${ancho}`);
+      this._nodoHtml.setAttribute("height",`${alto}`);
+    }
     this.Dibujar();
   }
   return rectángulo;
@@ -109,8 +161,12 @@ Mila.Lienzo._Lienzo.prototype._BorrarTodo = function() {
     Propósito: "Limpiar este lienzo."
   });
   if ('_nodoHtml' in this) {
-    let rectángulo = Mila.Geometria.rectánguloEn__De_x_(0, 0, this._nodoHtml.width, this._nodoHtml.height);
-    this._DibujarRectangulo_Con_(rectángulo, {borde:"#fff", fondo:"#fff"});
+    if (this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Canvas)) {
+      let rectángulo = Mila.Geometria.rectánguloEn__De_x_(0, 0, this._nodoHtml.width, this._nodoHtml.height);
+      this._DibujarRectangulo_Con_(rectángulo, {borde:"#fff", fondo:"#fff"});
+    } else { // this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Svg)
+      this._nodoHtml.replaceChildren();
+    }
   }
 };
 
@@ -127,79 +183,152 @@ Mila.Lienzo._Lienzo.prototype._DibujarElemento_ = function(elemento) {
 Mila.Lienzo._Lienzo.prototype._DibujarRectangulo_Con_ = function(rectángulo, atributos) {
   Mila.Contrato({
     Propósito: "Dibujar el rectángulo dado con los atributos dados en este lienzo.",
+    Precondiciones: [
+      "Se está ejecutando en el navegador.",
+      Mila.entorno().enNavegador()
+    ],
     Parámetros: [
       [rectángulo, Mila.Tipo.Rectángulo],
       [atributos, Mila.Tipo.AtributosDibujable]
     ]
   });
-  let contexto = this._contexto;
-  contexto.save();
-  contexto.beginPath();
-  if ('borde' in atributos) {
-    contexto.strokeStyle = atributos.borde;
-  }
-  if ('fondo' in atributos) {
-    contexto.fillStyle = atributos.fondo;
-    contexto.fillRect(
+  if (this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Canvas)) {
+    let contexto = this._contexto;
+    contexto.save();
+    contexto.beginPath();
+    if ('borde' in atributos) {
+      contexto.strokeStyle = atributos.borde;
+    }
+    if ('fondo' in atributos) {
+      contexto.fillStyle = atributos.fondo;
+      contexto.fillRect(
+        rectángulo.x,
+        rectángulo.y,
+        rectángulo.ancho,
+        rectángulo.alto
+      );
+    }
+    contexto.strokeRect(
       rectángulo.x,
       rectángulo.y,
       rectángulo.ancho,
       rectángulo.alto
     );
+    contexto.restore();
+  } else { // this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Svg)
+    const nodoRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    nodoRect.setAttribute("x", rectángulo.x);
+    nodoRect.setAttribute("y", rectángulo.y);
+    nodoRect.setAttribute("width", rectángulo.ancho);
+    nodoRect.setAttribute("height", rectángulo.alto);
+    if ('borde' in atributos) {
+      nodoRect.setAttribute("stroke", atributos.borde);
+    }
+    if ('fondo' in atributos) {
+      nodoRect.setAttribute("fill", atributos.fondo);
+    }
+    this._nodoHtml.appendChild(nodoRect);
   }
-  contexto.strokeRect(
-    rectángulo.x,
-    rectángulo.y,
-    rectángulo.ancho,
-    rectángulo.alto
-  );
-  contexto.restore();
 };
 
 Mila.Lienzo._Lienzo.prototype._DibujarCírculo_Con_ = function(círculo, atributos) {
   Mila.Contrato({
     Propósito: "Dibujar el círculo dado con los atributos dados en este lienzo.",
+    Precondiciones: [
+      "Se está ejecutando en el navegador.",
+      Mila.entorno().enNavegador()
+    ],
     Parámetros: [
       [círculo, Mila.Tipo.Círculo],
       [atributos, Mila.Tipo.AtributosDibujable]
     ]
   });
-  let contexto = this._contexto;
-  contexto.save();
-  contexto.beginPath();
-  if ('borde' in atributos) {
-    contexto.strokeStyle = atributos.borde;
+  if (this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Canvas)) {
+    let contexto = this._contexto;
+    contexto.save();
+    contexto.beginPath();
+    if ('borde' in atributos) {
+      contexto.strokeStyle = atributos.borde;
+    }
+    contexto.arc(círculo.x,círculo.y,círculo.r,0,2*Math.PI);
+    contexto.stroke();
+    if ('fondo' in atributos) {
+      contexto.fillStyle = atributos.fondo;
+      contexto.fill();
+    }
+    contexto.closePath();
+    contexto.restore();
+  } else { // this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Svg)
+    const nodoCirc = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    nodoCirc.setAttribute("x", círculo.x);
+    nodoCirc.setAttribute("y", círculo.y);
+    nodoCirc.setAttribute("r", círculo.r);
+    if ('borde' in atributos) {
+      nodoCirc.setAttribute("stroke", atributos.borde);
+    }
+    if ('fondo' in atributos) {
+      nodoCirc.setAttribute("fill", atributos.fondo);
+    }
+    this._nodoHtml.appendChild(nodoCirc);
   }
-  contexto.arc(círculo.x,círculo.y,círculo.r,0,2*Math.PI);
-  contexto.stroke();
-  if ('fondo' in atributos) {
-    contexto.fillStyle = atributos.fondo;
-    contexto.fill();
-  }
-  contexto.closePath();
-  contexto.restore();
 };
 
 Mila.Lienzo._Lienzo.prototype._DibujarSvg_ = function(svg) {
   Mila.Contrato({
     Propósito: "Dibujar el svg dado en este lienzo.",
+    Precondiciones: [
+      "Se está ejecutando en el navegador.",
+      Mila.entorno().enNavegador()
+    ],
     Parámetros: [
       [svg, Mila.Tipo.Svg]
     ]
   });
-  let contexto = this._contexto;
-  contexto.save();
-  contexto.beginPath();
-  const datos = new Path2D(svg.comandosComoTexto());
-  contexto.lineWidth = svg.grosorLínea()
-  contexto.strokeStyle = svg.colorLínea();
-  contexto.stroke(datos);
-  contexto.globalAlpha = svg.opacidadFondo();
-  contexto.fillStyle = svg.colorFondo();
-  contexto.fill(datos);
-  contexto.closePath();
-  contexto.restore();
-  svg.hijos().conCadaUno(hijo => this._DibujarSvg_(hijo));
+  if (this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Canvas)) {
+    let contexto = this._contexto;
+    contexto.save();
+    contexto.beginPath();
+    const datos = new Path2D(svg.comandosComoTexto());
+    contexto.lineWidth = svg.grosorLínea()
+    contexto.strokeStyle = svg.colorLínea();
+    contexto.stroke(datos);
+    contexto.globalAlpha = svg.opacidadFondo();
+    contexto.fillStyle = svg.colorFondo();
+    contexto.fill(datos);
+    contexto.closePath();
+    contexto.restore();
+    svg.hijos().conCadaUno(hijo => this._DibujarSvg_(hijo));
+  } else { // this._modoHtml.esIgualA_(Mila.Lienzo.ModoHtmlLienzo.Svg)
+    this._nodoHtml.appendChild(Mila.Lienzo._nodoSvgAPartirDe_(svg));
+  }
+};
+
+Mila.Lienzo._nodoSvgAPartirDe_ = function(svg) {
+  Mila.Contrato({
+    Propósito: "Describe un nodo html para el svg dado.",
+    Precondiciones: [
+      "Se está ejecutando en el navegador.",
+      Mila.entorno().enNavegador()
+    ],
+    Parámetros: [
+      [svg, Mila.Tipo.Svg]
+    ]
+  });
+  let claseNodo = 'path';
+  if (svg.comandos().esVacia()) {
+    claseNodo = 'g';
+  }
+  const nodo = document.createElementNS("http://www.w3.org/2000/svg", claseNodo);
+  if (!svg.comandos().esVacia()) {
+    nodo.setAttribute("d", svg.comandosComoTexto());
+  }
+  nodo.setAttribute("stroke", svg.colorLínea());
+  nodo.setAttribute("fill", svg.colorFondo());
+  nodo.setAttribute("fill-opacity", svg.opacidadFondo());
+  svg.hijos().conCadaUno(x => {
+    nodo.appendChild(Mila.Lienzo._nodoSvgAPartirDe_(x));
+  });
+  return nodo;
 };
 
 Mila.Tipo.Registrar({
--- src/pantalla/escenario.js
+++ src/pantalla/escenario.js
@@ -31,7 +31,7 @@ Mila.Pantalla.nuevoEscenario = function(atributos={}) {
     grosorBorde:1, colorBorde:"#000"
   });
   nuevoEscenario._lienzo = Mila.Lienzo.nuevo();
-  nuevoEscenario._contenidoHtml = [];
+  nuevoEscenario._lienzo.CambiarModoHtmlA_(Mila.Lienzo.ModoHtmlLienzo.Svg);
   nuevoEscenario.CambiarEscenaA_(atributos.escena);
   nuevoEscenario.CambiarCámaraA_(atributos.cámara);
   return nuevoEscenario;
@@ -48,11 +48,6 @@ Mila.Pantalla._Escenario.prototype.CambiarEscenaA_ = function(nuevaEscena) {
     ]
   });
   this._escena = nuevaEscena;
-  this._contenidoHtml = nuevaEscena
-    .contenido()
-    .losQueCumplen(elemento => elemento.elementoOriginal.esDeTipo_(Mila.Tipo.ElementoVisual))
-    .transformados(elemento => elemento.elementoOriginal)
-  ;
 };
 
 Mila.Pantalla._Escenario.prototype.CambiarCámaraA_ = function(nuevaCámara) {
@@ -96,22 +91,16 @@ Mila.Pantalla._Escenario.prototype._Refrescar = function() {
   const contenidoLienzo = [];
   this._escena.contenido().conCadaUno(function(elementoEscena) {
     const elemento = elementoEscena.elementoOriginal;
-    if (elemento.esDeTipo_(Mila.Tipo.ElementoVisual)) {
-      elemento.CambiarPosiciónXA_(elementoEscena.campo_ConIndirección('x') - cámara.posición().x);
-      elemento.CambiarPosiciónYA_(elementoEscena.campo_ConIndirección('y') - cámara.posición().y);
-      // ¿zoom?
-    } else if (elemento.esDibujable()) {
-      const dibujable = Mila.Lienzo.copiaDeDibujable_(elemento);
-      let s = dibujable.defineLaClave_('s') ? dibujable.s : 1;
-      if (dibujable.defineLaClave_('x')) {
-        dibujable.x = (elementoEscena.campo_ConIndirección('x') - cámara.posición().x) * cámara.zoom()/100/s;
-      }
-      if (dibujable.defineLaClave_('y')) {
-        dibujable.y = (elementoEscena.campo_ConIndirección('y') - cámara.posición().y) * cámara.zoom()/100/s;
-      }
-      Mila.Lienzo.EscalarDibujable_En_(dibujable, cámara.zoom()/100);
-      contenidoLienzo.push(dibujable);
+    const dibujable = Mila.Lienzo.copiaDeDibujable_(elemento);
+    let s = dibujable.defineLaClave_('s') ? dibujable.s : 1;
+    if (dibujable.defineLaClave_('x')) {
+      dibujable.x = (elementoEscena.campo_ConIndirección('x') - cámara.posición().x) * cámara.zoom()/100/s;
     }
+    if (dibujable.defineLaClave_('y')) {
+      dibujable.y = (elementoEscena.campo_ConIndirección('y') - cámara.posición().y) * cámara.zoom()/100/s;
+    }
+    Mila.Lienzo.EscalarDibujable_En_(dibujable, cámara.zoom()/100);
+    contenidoLienzo.push(dibujable);
   });
   this._lienzo.CambiarContenidoA_(contenidoLienzo);
   this._lienzo.Dibujar();
@@ -143,7 +132,6 @@ Mila.Pantalla._Escenario.prototype.PlasmarEnHtml = function(nodoMadre) {
     ]
   });
   this._lienzo.PlasmarEnHtml(nodoMadre);
-  this._contenidoHtml.conCadaUno(elemento => elemento.PlasmarEnHtml(nodoMadre));
   this._Refrescar();
 };
 
